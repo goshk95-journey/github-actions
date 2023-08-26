51b3580a146c0fff3bdc9b4aec79c2b8858fe86c
Gosh95 <pocal777@naver.com>
2023-08-26 17:04:54 +0900

    테스트 성공 케이스 풀 리퀘스트

---
+++ src/test/java/com/example/githubactions/demo/DemoTest.java
@@ -0,0 +1,15 @@
+package com.example.githubactions.demo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class DemoTest {
+
+  boolean isTrue = true;
+
+  @Test
+  void succeed() {
+    assertThat(isTrue).isTrue();
+  }
+}
